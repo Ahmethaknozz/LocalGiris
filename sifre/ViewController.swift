4fc9b87d6c447c7c75fb82cd610ab856451132d4
Ahmethaknozz <121724265+Ahmethaknozz@users.noreply.github.com>
2023-02-17 17:16:18 +0300

    Initial Commit

---
+++ sifre/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sifre
+//
+//  Created by ahmet hakan öz on 17.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
